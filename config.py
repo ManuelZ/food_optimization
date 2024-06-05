a684924aaa6c27b5cb23c9fa7a34f6f7e14ca7a7
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-06-04 22:42:41 -0500

    Move data to a different folder

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    DB_PATH: str = "ABBREV.csv"
-    CUSTOM_DB_PATH: str = "custom_foods.csv"
-    EXTRA_INFO_PATH: str = "extra_info.csv"
+    DB_PATH: str = "data/ABBREV.csv"
+    CUSTOM_DB_PATH: str = "data/custom_foods.csv"
+    EXTRA_INFO_PATH: str = "data/extra_info.csv"
     SOLVER_NAME: str = "SCIP_MIXED_INTEGER_PROGRAMMING"  # 'GLOP'
     INFINITY: int = 999999
     N_DECISION_VARS_PER_FOOD: int = 4
